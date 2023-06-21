319751d29bdf5a23b47af4f2c1b81e3a41ace0de
JericLew <lew.jeric@gmail.com>
2023-06-21 14:29:39 +0800

    arg parser for python script for .mat

--- training/matlab_convertor/matlab_convertor.py
+++ training/matlab_convertor/matlab_convertor.py
@@ -2,12 +2,30 @@ import scipy.io as sio
 import numpy as np
 import pandas as pd
 import csv
+import argparse
 
-data = sio.loadmat('/home/jeric/VIS_Onboard/ObjectGT/MVI_0790_VIS_OB_ObjectGT.mat')
+# Create the argument parser
+parser = argparse.ArgumentParser(description='Convert .mat to .csv to multiple .txt')
+
+# Add the input and output file arguments
+parser.add_argument('input_mat_path', help='Input .mat file path')
+parser.add_argument('output_folder_path', help='Output folder path')
+
+# Parse the arguments
+args = parser.parse_args()
+
+frame_count = 0
+input_mat_path = args.input_mat_path
+video_name = input_mat_path.split('/')[-1].split('_ObjectGT.mat')[0]
+output_folder_path = args.output_folder_path
+output_csv = output_folder_path + video_name + ".csv"
+output_txt = output_folder_path + video_name + '_' + str(frame_count) + '.txt'
+
+data = sio.loadmat(input_mat_path)
 print(data.keys())
 
 # Get variable information from the .mat file
-variables = sio.whosmat('/home/jeric/VIS_Onboard/ObjectGT/MVI_0790_VIS_OB_ObjectGT.mat')
+variables = sio.whosmat(input_mat_path)
 
 print(variables)
 
@@ -33,8 +51,7 @@ num_fields = len(field_names)
 
 def write_csv():
     # Open the output CSV file for writing
-    output_file = 'output.csv'
-    with open(output_file, 'w', newline='') as file:
+    with open(output_csv, 'w', newline='') as file:
         writer = csv.writer(file)
 
         # Write the header row with the field names
@@ -44,31 +61,32 @@ def write_csv():
         for i in range(struct_data.shape[1]):
 
             # to extract type and bbox 
-            if data_rows[i][1][0]:
+            if data_rows[i][1].size > 0:
                 extracted_data = [data_rows[i][1][0][0], data_rows[i][6][0][0], data_rows[i][6][0][1] ,data_rows[i][6][0][2] ,data_rows[i][6][0][3]]
             else:
                 extracted_data = [0, 0, 0, 0, 0]
             writer.writerow(extracted_data) 
 
-            
             # # to extract all data
             # writer.writerow(data_rows[i][j] for j in range(len(field_names))) 
-    print(f"Conversion complete. Saved as {output_file}")
+    print(f"Conversion to csv complete.")
 
 def convert_txt():
-    input_csv = 'output.csv'
-    output_txt = 'output.txt'
-
-    with open(input_csv, 'r') as file:
+    with open(output_csv, 'r') as file:
         csv_reader = csv.reader(file)
         rows = list(csv_reader)
-
-    with open(output_txt, 'w') as file:
-        for row in rows:
+    
+    # Skip the first line
+    rows = rows[1:]
+    
+    for row in rows:
+        with open(output_txt, 'w') as file:
             line = ' '.join(row)
             file.write(line + '\n')
+        global frame_count
+        frame_count += 1
 
-    print(f"Conversion complete. Saved as {output_txt}")
+    print(f"Conversion to txt complete")
 
 write_csv()
 convert_txt()
